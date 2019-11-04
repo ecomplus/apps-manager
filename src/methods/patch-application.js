dd78f2425b17fdb78948951b542aaa4e19a2ae65
Talisson Ferreira <talissonf@gmail.com>
2019-11-04 19:55:21 -0300

    feat(patch-application): update app body if data and hidden is not sent

--- src/methods/patch-application.js
+++ src/methods/patch-application.js
@@ -1,13 +1,19 @@
-export default (self, _id, { data, hiddenData }) => {
+export default (self, _id, body) => {
   const method = 'patch'
   const promises = []
   const { ecomAuth } = self
-  if (data) {
+  const { data, hidden_data } = body
+
+  if (data && typeof data === 'object') {
     promises.push(ecomAuth.apiRequest(`/applications/${_id}/data.json`, method, data))
   }
 
-  if (hiddenData) {
-    promises.push(ecomAuth.apiRequest(`/applications/${_id}/hidden_data.json`, method, hiddenData))
+  if (hidden_data && typeof hidden_data === 'object') {
+    promises.push(ecomAuth.apiRequest(`/applications/${_id}/hidden_data.json`, method, hidden_data))
+  }
+
+  if (!data && !hidden_data && body) {
+    promises.push(ecomAuth.apiRequest(`/applications/${_id}.json`, method, body))
   }
 
   return Promise.all(promises)
