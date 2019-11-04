7385323653d1dae5830e6f918654caa14617ade0
Talisson Ferreira <talissonf@gmail.com>
2019-11-04 16:19:51 -0300

    feat: setup methods

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@
  * @author E-Com Club <ti@e-com.club>
  * @license AGPL-3.0
  */
+import EcomAuth from '@ecomplus/auth'
+import fetchApplications from './methods/fetch-applications'
+import findApplications from './methods/find-applications'
+import installApplication from './methods/install-application'
+import removeApplication from './methods/remove-application'
+import patchApplication from './methods/patch-application'
 
-const EcomApps = function () {
+const EcomApps = function (sessionKey) {
+  const self = this
+  this.ecomAuth = new EcomAuth(sessionKey)
+  this.fetch = (meta = {}) => fetchApplications(meta)
+  this.find = appId => findApplications(appId)
+  this.install = appId => installApplication(self, appId)
+  this.remove = _id => removeApplication(self, _id)
+  this.patch = (_id, { data, hiddenData }) => patchApplication(self, _id, { data, hiddenData })
 }
 
 export default EcomApps
