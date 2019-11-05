5e75da2fe440395354add04c333cd4abfbd2b466
Talisson Ferreira <talissonf@gmail.com>
2019-11-05 10:59:57 -0300

    Update src/methods/remove-application.js
    Co-Authored-By: Leonardo Matos <leomp120894@gmail.com>

--- src/methods/remove-application.js
+++ src/methods/remove-application.js
@@ -1,1 +1,1 @@
-export default (self, _id) => self.ecomAuth.apiRequest(`/applications/${_id}.json`, 'delete')
+export default (self, _id) => self.ecomAuth.requestApi(`/applications/${_id}.json`, 'delete')
