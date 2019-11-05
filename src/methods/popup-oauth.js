44f163312d703ca7f2c945734b1c4015f5939ab5
Talisson Ferreira <talissonf@gmail.com>
2019-11-05 11:54:53 -0300

    perf(create-popup): transformed method into internal function

--- src/methods/popup-oauth.js
+++ src/methods/popup-oauth.js
@@ -1,5 +1,3 @@
-import createPopup from '../lib/create-popup'
-
 export default url => {
   const popup = createPopup(url)
   if (popup) {
@@ -8,4 +6,26 @@ export default url => {
     }
   }
   return popup
+}
+
+const createPopup = (url, title = 'Authentication') => {
+  if (typeof window !== 'object') {
+    throw new Error('Method available for browser only')
+  }
+
+  const { screen } = window
+  let width
+  if (screen) {
+    // set window width based on screen width
+    if (screen.width >= 640) {
+      width = 900
+    } else {
+      width = screen.width
+    }
+  } else {
+    width = 380
+  }
+
+  // open new browser window
+  return window.open(url, title, `height=500,width=${width}`)
 }
