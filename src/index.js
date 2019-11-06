75e95989fabb3efef49953a1e1bf1a8aea2d6517
Leonardo Matos <leomp120894@gmail.com>
2019-11-06 14:18:03 -0300

    docs(constructor): add module and class properly on jsdoc reference

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@
  * https://github.com/ecomclub/ecomplus-auth
  * @author E-Com Club <ti@e-com.club>
  * @license AGPL-3.0
- *
- * @description
+ */
+
+import EcomAuth from '@ecomplus/auth'
+import fetchApps from './methods/fetch-apps'
+import findApp from './methods/find-app'
+import installApp from './methods/install-app'
+import removeApplication from './methods/remove-application'
+import editApplication from './methods/edit-application'
+
+/**
  * JS client for manage E-Com Plus Apps
  * @module @ecomplus/apps-manager
- * @namespace EcomApps
  * @see EcomApps
  *
  * @example
@@ -21,13 +28,6 @@
  * <!-- Global `@ecomplus/apps-manager` from CDN on browser -->
  * <script src="https://cdn.jsdelivr.net/npm/@ecomplus/apps-manager@0.1/dist/ecom-apps.root.min.js"></script>
  */
-
-import EcomAuth from '@ecomplus/auth'
-import fetchApps from './methods/fetch-apps'
-import findApp from './methods/find-app'
-import installApp from './methods/install-app'
-import removeApplication from './methods/remove-application'
-import editApplication from './methods/edit-application'
 
 const EcomApps = function (ecomAuth = new EcomAuth()) {
   const self = this
