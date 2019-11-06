4b801054c30bf9603da06f943dd01f5631ce4883
Leonardo Matos <leomp120894@gmail.com>
2019-11-06 13:24:40 -0300

    fix(fetch-apps): fix concating querystring

--- src/methods/fetch-apps.js
+++ src/methods/fetch-apps.js
@@ -1,17 +1,19 @@
 import request from '../lib/market-requests'
+
 /**
  * @method
  * @memberof EcomApps
  * @name fetchApps
- * @description fetch list of apps from Market API
+ * @description Fetch list of apps from Market API
  *
  * @param {object} [meta] - Search options
- * @param {object} [meta.params] - search parameters, can be {title},{slug},{category}
+ * @param {object} [meta.params] - Search filters, can be `{ title, slug, category }`
  * @param {number} [meta.limit=30] - Set the maximum number of results to return
  * @param {number} [meta.offset=0] - Specifies the first entry to return
  * @param {array} [meta.fields] - Specifies the object properties to return
  *
  * @returns {Promise<response|error>}
+ *
  * @example
 // retrieve simple list of apps
 ecomApps.fetchApps()
@@ -51,22 +53,20 @@ ecomApps.fetchApps(options)
   .then(result => console.log(result))
   .catch(e => console.log(e))
 */
+
 export default (query) => {
   const { params, limit, offset, fields } = query
+
   let url = '/applications?'
-
-  if (limit) {
-    url += `&limit=${limit}`
-  }
-
-  if (offset) {
-    url += `&offset=${offset}`
-  }
-
   if (params) {
     url += Object.keys(params).map(key => key + '=' + params[key]).join('&')
   }
-
+  if (limit) {
+    url += `&limit=${limit}`
+  }
+  if (offset) {
+    url += `&offset=${offset}`
+  }
   if (fields && Array.isArray(fields) && fields.length) {
     url += `&fields=${fields.join()}`
   }
