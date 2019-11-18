48f878192904cdecda0055f8e5d1f4c8ff002980
Talisson Ferreira <talissonf@gmail.com>
2019-11-18 16:17:03 -0300

    feat(fetch-store-applications): pagination, ordering and querying

--- src/methods/fetch-store-applications.js
+++ src/methods/fetch-store-applications.js
@@ -4,9 +4,13 @@
  * @name fetchStoreApplications
  * @description Fetch merchant's applications list
  * [Store API]{@link https://developers.e-com.plus/docs/api/#/store/applications}.
- * @param {number} [appId] - Specifies application id to search
- * @param {array} [fields] - Specifies the object properties to return
  *
+ * @param {object} [meta] - Search options
+ * @param {object} [meta.params] - Search filters, can be `{ _id, app_id, title, slug, category, state, version, type }`
+ * @param {number} [meta.limit=30] - Set the maximum number of results to return
+ * @param {number} [meta.offset=0] - Specifies the first entry to return
+ * @param {array} [meta.fields] - Specifies the object properties to return
+ * @param {string} [meta.sort] - Specifies rules to order the resultant objects
  * @returns {Promise<data|error>}
  *
  * @example
@@ -18,13 +22,30 @@ ecomApps.fetchStoreApplications()
  */
 
 export default (self, meta = {}) => {
-  const { appId, fields } = meta
+  const { params, fields, limit, offset, sort } = meta
   let url = '/applications.json?'
-  if (appId) {
-    url += `app_id=${appId}`
+  if (params) {
+    for (const key in params) {
+      switch (typeof params[key]) {
+        case 'string':
+        case 'number':
+          url += `${key}=${params[key]}&`
+      }
+    }
+  }
+  if (limit) {
+    url += `&limit=${limit}`
+  }
+  if (offset) {
+    url += `&offset=${offset}`
+  }
+  if (sort) {
+    url += `&sort=${sort}`
   }
   if (fields && Array.isArray(fields) && fields.length) {
     url += `&fields=${fields.join()}`
+  } else if (fields && typeof fields === 'string') {
+    url += fields
   }
   return self.ecomAuth.requestApi(url).then(({ data }) => data.result)
 }
