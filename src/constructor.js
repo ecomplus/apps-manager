1607b32e5f4864b8b3b1c4354fb889388aef34d7
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2021-01-18 23:53:57 -0300

    feat: convert to export instance of EcomApps

--- src/constructor.js
+++ src/constructor.js
@@ -4,7 +4,7 @@
  * @license AGPL-3.0
  */
 
-import ecomAuth from '@ecomplus/auth'
+import { ecomAuth } from '@ecomplus/auth'
 import listFromMarket from './methods/list-from-market'
 import findOnMarket from './methods/find-on-market'
 import install from './methods/install'
@@ -12,6 +12,7 @@ import remove from './methods/remove'
 import edit from './methods/edit'
 import list from './methods/list'
 import find from './methods/find'
+import ecomApps from '.'
 
 /**
  * JS client for manage E-Com Plus Apps
@@ -31,13 +32,43 @@ import find from './methods/find'
  * <script src="https://cdn.jsdelivr.net/npm/@ecomplus/apps-manager@0.1/dist/ecom-apps.root.min.js"></script>
  */
 
-import EcomApps from './constructor'
+export default class EcomApps {
+  custructor() {
+    this.ecomAuth = ecomAuth
+  }
 
-const ecomApps = new EcomApps()
+  async listFromMarket(meta) {
+    listFromMarket(ecomApps, meta)
+  }
 
-export default ecomApps
+  async findOnMarket(appId) {
+    findOnMarket(ecomApps, appId)
+  }
 
-export { ecomApps, EcomApps}
+  async install(appId, redirect, body) {
+    install(ecomApps, appId, redirect, body)
+  }
+
+  async remove(_id) {
+    remove(ecomApps, _id)
+  }
+
+  async edit(_id, body, canUpdateInternals) {
+    edit(ecomApps, _id, body, canUpdateInternals)
+  }
+
+  async list(meta) {
+    list(ecomApps, meta)
+  }
+
+  async find(_id) {
+    find(ecomApps, _id)
+  }
+
+  async listFromMarket(meta) {
+    return listFromMarket(ecomApps, meta)
+  }
+}
 
 /**
  * Construct a new apps manager instance object.
